dfbbfc3463396a381dd05b21c5647577bc314719
Saikat Das <saikatdas0790@gmail.com>
2020-09-16 15:39:56 +0530

    deploy to npm

---
+++ index.js
@@ -0,0 +1,12 @@
+export default (
+  filterPrefix = "SVELTE_APP_",
+  targetPrefix = "process.env.",
+  excluded = [],
+) => {
+  const FRONTEND_APP_ENV_VARS = {};
+  for (let key in process.env) {
+    if (key.includes(filterPrefix) && !excluded.includes(key))
+      FRONTEND_APP_ENV_VARS[targetPrefix + key] = "'" + process.env[key] + "'";
+  }
+  return FRONTEND_APP_ENV_VARS;
+};
